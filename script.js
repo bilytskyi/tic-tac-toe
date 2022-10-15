2c7be9c77036e426cc99e8bbfb5f6d639d0caf50
Andrii Bilytskyi <abilytskyi@gmail.com>
2022-10-15 22:04:14 +0200

    refactoring + basic win condition + check if cell already have a mark

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
 const gameBoard = (() => {
 
-    // let board = ["X", "X", "X",
-    //              "O", "O", "O",
-    //              "X", "X", "X"];
-    
     let board = ["", "", "",
                  "", "", "",
                  "", "", ""]; 
 
     const divBoard = document.querySelector('.board');
-
-    const add = (a, b) => a + b;
-    const sub = (a, b) => a - b;
 
     const setBoard = (board) => {
         for (let i = 0; i < board.length; i++) {
@@ -26,49 +19,28 @@ const gameBoard = (() => {
     const displayBoard = () => setBoard(board);
 
     const displayMark = (mark, position) => {
-        // divBoard.innerHTML = "";
-        board[position] = mark;
+
         const selector = `.cell[id="${position}"]`;
         const cellWithThisPosition = document.querySelector(selector);
+        
+        if (board[position] == '') {
+            board[position] = mark;
+            cellWithThisPosition.textContent = mark;
+        }
 
-        cellWithThisPosition.textContent = mark;
+        if (board[0] == 'X' && board[1] == 'X' && board[2] == 'X') { // basic win condition
+            alert("Hey!! You WIN!!!");
+        }
 
-        // displayBoard(board);
     }
 
-    // const test3 = (divCells) => {
-    //     divCells.forEach(cell => {
-    //         cell.addEventListener('click', (event) => {
-    //             alert('s');
-    //             test2("X", event.target.id);
-    //         });
-    //     });
-    // }
-
     return {
-        add,
-        sub,
         setBoard,
         displayBoard,
-        // test1,
         displayMark,
     };
+
 })();
-
-// gameBoard.test2('X', 2)
-// gameBoard.test2('X', 3)
-
-// const buttons = document.querySelectorAll('button');
-// const text = document.querySelector('p');
-
-// buttons.forEach(button => {
-//     button.addEventListener('click', (event) => {
-//         text.textContent = `${event.target.id}`
-//     })
-// })
-
-const divCells = document.querySelectorAll('cell');
-
 
 const Player = (mark) => {
     const getMark = () => mark;
@@ -81,55 +53,41 @@ const Player = (mark) => {
     return {getMark, move};
 };
 
-const xPlayer = Player('X');
-const yPlayer = Player('O');
-
-// xPlayer.move(0);
-// yPlayer.move(5);
-
 const displayController = (() => {
 
-    const add = (a, b) => a + b;
-    const sub = (a, b) => a - b;
-
-    const play1 = () => {
-        const buttons = document.querySelectorAll('button');
-        const text = document.querySelector('p');
-        buttons.forEach(button => {
-            button.addEventListener('click', (event) => {
-                text.textContent = `${event.target.id}`
-            });
-        });
-
-    }
-
-    const play2 = () => {
+    const play = () => {
+        gameBoard.displayBoard();
         const cells = document.querySelectorAll('.cell');
-        const text = document.querySelector('p');
+        const displayId = document.querySelector('.display-id');
+        const displayPlayerTurn = document.querySelector('.display-player-turn');
+        let turn = 1;
         cells.forEach(cell => {
             cell.addEventListener('click', (event) => {
-                text.textContent = `${event.target.id}`
-                xPlayer.move(event.target.id);
+                displayId.textContent = `${event.target.id}`;
+                displayPlayerTurn.textContent = `${turn}`;
+
+                if (turn == 1) {
+                    turn += 1;
+                    xPlayer.move(event.target.id);
+                }
+
+                else {
+                    turn -= 1;
+                    yPlayer.move(event.target.id);
+                }
+
             });
         });
 
     }
 
     return {
-        add,
-        sub,
-        play1,
-        play2,
+        play,
     };
 
 })();
 
-// displayController.play1();
+const xPlayer = Player('X');
+const yPlayer = Player('O');
 
-// displayController.play2();
-
-
-gameBoard.displayBoard();
-xPlayer.move(0);
-xPlayer.move(5);
-displayController.play2();
+displayController.play();
