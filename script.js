bbc4323fbc688ef8482d64838dba85a38eb38c7c
Andrii Bilytskyi <abilytskyi@gmail.com>
2022-10-15 20:58:26 +0200

    add players and basic func, now need displayController

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const gameBoard = (() => {
 
-    let board = ["X", "X", "X",
-                 "O", "O", "O",
-                 "X", "X", "X"];  
+    // let board = ["X", "X", "X",
+    //              "O", "O", "O",
+    //              "X", "X", "X"];
+    
+    let board = ["", "", "",
+                 "", "", "",
+                 "", "", ""]; 
 
     const divBoard = document.querySelector('.board');
 
@@ -14,16 +18,68 @@ const gameBoard = (() => {
             const divCell = document.createElement('div');
             divCell.textContent = board[i];
             divCell.className = 'cell';
+            divCell.setAttribute('id', i);
             divBoard.appendChild(divCell);
         }
     }
+
+    // const test1 = () => displayBoard(board);
+
+    const displayMark = (mark, position) => {
+        divBoard.innerHTML = "";
+        board[position] = mark;
+        displayBoard(board);
+    }
+
+    // const test3 = (divCells) => {
+    //     divCells.forEach(cell => {
+    //         cell.addEventListener('click', (event) => {
+    //             alert('s');
+    //             test2("X", event.target.id);
+    //         });
+    //     });
+    // }
 
     return {
         add,
         sub,
         board,
         displayBoard,
+        // test1,
+        displayMark,
     };
 })();
 
-gameBoard.displayBoard(gameBoard.board);
+// gameBoard.test2('X', 2)
+// gameBoard.test2('X', 3)
+
+const buttons = document.querySelectorAll('button');
+const text = document.querySelector('p');
+
+buttons.forEach(button => {
+    button.addEventListener('click', (event) => {
+        text.textContent = `${event.target.id}`
+    })
+})
+
+const divCells = document.querySelectorAll('cell');
+
+
+const Player = (mark) => {
+    const getMark = () => mark;
+
+    const move = (position) => {
+        gameBoard.displayMark(getMark(), position)
+    }
+
+
+    return {getMark, move};
+};
+
+const xPlayer = Player('X');
+const yPlayer = Player('O');
+
+text.textContent = xPlayer.getMark();
+
+xPlayer.move(0);
+yPlayer.move(5);
